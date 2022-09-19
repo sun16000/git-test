d294d9a3969630a70569235df464224f8cae6fdb
SXD <1600013725@qq.com>
2022-09-19 09:49:52 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello, Gittttttt~");
         System.out.println("Hello, Git 2");
+        System.out.println("Hello, Git 3");
     }
 
 }
