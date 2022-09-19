dcc498b6d32da32edcb7a89f4e1eedc791f37d31
SXD <1600013725@qq.com>
2022-09-19 09:41:43 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author SXD
+ * @date 2022-09-19 9:36
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Gittttttt~");
+    }
+
+}
